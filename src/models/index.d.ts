fb5fd8643cab2bfaca80bac6d71d385e325af7f2
SamMaher <samsmaher@gmail.com>
2021-05-26 17:43:59 +1000

    Implemented backend

--- src/models/index.d.ts
+++ src/models/index.d.ts
@@ -8,6 +8,7 @@ export declare class Friend {
   readonly id: string;
   readonly Name?: string;
   readonly Url?: string;
+  readonly Index?: number;
   constructor(init: ModelInit<Friend>);
   static copyOf(source: Friend, mutator: (draft: MutableModel<Friend>) => MutableModel<Friend> | void): Friend;
 }
@@ -18,6 +19,7 @@ export declare class Gig {
   readonly Url?: string;
   readonly DateName?: string;
   readonly DayName?: string;
+  readonly DateTime?: string;
   constructor(init: ModelInit<Gig>);
   static copyOf(source: Gig, mutator: (draft: MutableModel<Gig>) => MutableModel<Gig> | void): Gig;
 }
